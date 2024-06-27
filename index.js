135fd4f728baaa164f36ad43ea379d026df17a23
Agus Sugandi <agus.sugandi95@gmail.com>
2024-06-27 19:09:43 +0700

    add ai

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const { makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys");
 const qrcode = require('qrcode-terminal')
+const pino = require('pino');
+
 
 async function connectToWhatsApp(){
     const auth = await useMultiFileAuthState("session")
     const socket = makeWASocket({
         printQRTerminal : true, 
-        Browser : ["Windows 11", "Chrome", "11"], 
+        Browser : ["Windows", "Chrome", "11"], 
         auth : auth.state, 
         logger: pino({ level: "silent" }),
     })
@@ -27,7 +29,30 @@ async function connectToWhatsApp(){
         console.log(messages)
         const chat = messages[0].message?.extendedTextMessage?.text || messages[0].message?.conversation;
         const fromNumberHp = messages[0].key.remoteJid
+        console.log(messages[0].key.fromMe)
+        if( !messages[0].key.fromMe ){
+            query({"question": chat }).then((response) => {
+                const { text, question, chatId, chatMessageId, sessionId } = response
+                socket.sendMessage(fromNumberHp, { text: text })
+            });
+        }
+
     })
+}
+
+async function query(data) {
+    const response = await fetch(
+        "http://localhost:3000/api/v1/prediction/37cd82fa-b234-4420-98cd-b51fd52f3193",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
+    );
+    const result = await response.json();
+    return result;
 }
 
 connectToWhatsApp()
